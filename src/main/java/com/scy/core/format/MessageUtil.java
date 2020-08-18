7fddc80599cb182575ecc5163f1cf79f9a8fb408
史春阳 <shichunyang02@meituan.com>
2020-08-18 13:45:14 +0800

    MessageFormatUtil.java

---
+++ src/main/java/com/scy/core/format/MessageUtil.java
@@ -0,0 +1,13 @@
+package com.scy.core.format;
+
+/**
+ * MessageUtil
+ *
+ * @author shichunyang
+ * Created by shichunyang on 2020/8/18.
+ */
+public class MessageUtil {
+
+    private MessageUtil() {
+    }
+}
