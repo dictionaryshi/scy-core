40107b9f60bc0355f6d08693a3f93e7ec0bd3e26
史春阳 <shichunyang02@meituan.com>
2020-08-25 18:05:46 +0800

    ThreadPoolUtil.java

---
+++ src/main/java/com/scy/core/thread/TransmittableThreadPoolExecutor.java
@@ -0,0 +1,38 @@
+package com.scy.core.thread;
+
+import com.alibaba.ttl.TtlRunnable;
+import com.scy.core.trace.TraceUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * TransmittableThreadPoolExecutor
+ *
+ * @author shichunyang
+ * Created by shichunyang on 2020/8/25.
+ */
+@Slf4j
+public class TransmittableThreadPoolExecutor extends ThreadPoolExecutor {
+
+    public TransmittableThreadPoolExecutor(
+            int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler
+    ) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    public void beforeExecute(Thread thread, Runnable runnable) {
+        TraceUtil.setMdcTraceId();
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+        super.execute(TtlRunnable.get(runnable));
+    }
+
+    @Override
+    public void afterExecute(Runnable runnable, Throwable throwable) {
+        TraceUtil.clearMdc();
+    }
+}
