5adf553c838e4f1f2205a3208d7454cc08afe21f
史春阳 <shichunyang02@meituan.com>
2020-08-29 20:05:25 +0800

    ParamUtil.java

--- src/main/java/com/scy/core/ParamUtil.java
+++ src/main/java/com/scy/core/ParamUtil.java
@@ -1,9 +1,12 @@
 package com.scy.core;
 
+import com.scy.core.format.MessageUtil;
 import com.scy.core.reflect.ClassUtil;
+import com.scy.core.spring.ApplicationContextUtil;
 import com.scy.core.spring.AutoConfigurationPackageUtil;
-import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 /**
@@ -12,12 +15,32 @@ import java.util.List;
  * @author shichunyang
  * Created by shichunyang on 2020/8/28.
  */
+@Slf4j
 public class ParamUtil {
 
     private ParamUtil() {
     }
 
-    public static boolean isBasicParam(Class<?> paramClass, ApplicationContext applicationContext) {
+    public static boolean isStartWithBasePackage(Class<?> paramClass) {
+        String className = paramClass.getName();
+        if (className.startsWith(SystemUtil.BASE_PACKAGE)) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+
+    public static boolean isStartWithAutoConfigurationPackage(Class<?> paramClass) {
+        String className = paramClass.getName();
+        List<String> autoConfigurationPackages = AutoConfigurationPackageUtil.getAutoConfigurationPackage(ApplicationContextUtil.getApplicationContext());
+        for (String autoConfigurationPackage : autoConfigurationPackages) {
+            if (className.startsWith(autoConfigurationPackage)) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    public static boolean isBasicParam(Class<?> paramClass) {
         if (ClassUtil.isAssignable(String.class, paramClass)) {
             return Boolean.TRUE;
         }
@@ -26,20 +49,42 @@ public class ParamUtil {
             return Boolean.TRUE;
         }
 
-        String className = paramClass.getName();
-        if (className.startsWith(SystemUtil.BASE_PACKAGE)) {
+        if (isStartWithBasePackage(paramClass)) {
             return Boolean.TRUE;
         }
 
-        if (!ObjectUtil.isNull(applicationContext)) {
-            List<String> autoConfigurationPackages = AutoConfigurationPackageUtil.getAutoConfigurationPackage(applicationContext);
-            for (String autoConfigurationPackage : autoConfigurationPackages) {
-                if (className.startsWith(autoConfigurationPackage)) {
-                    return Boolean.TRUE;
-                }
-            }
+        if (isStartWithAutoConfigurationPackage(paramClass)) {
+            return Boolean.TRUE;
         }
 
         return Boolean.FALSE;
+    }
+
+    public static void trimParam(Object param) {
+        if (ObjectUtil.isNull(param)) {
+            return;
+        }
+
+        Class<?> paramClass = param.getClass();
+        if (!isStartWithBasePackage(paramClass) && !isStartWithAutoConfigurationPackage(paramClass)) {
+            return;
+        }
+
+        Field[] fields = paramClass.getDeclaredFields();
+        for (Field field : fields) {
+            if (!ObjectUtil.equals(field.getType(), String.class)) {
+                continue;
+            }
+            field.setAccessible(Boolean.TRUE);
+            try {
+                String value = (String) field.get(param);
+                if (ObjectUtil.isNull(value)) {
+                    continue;
+                }
+                field.set(param, value.trim());
+            } catch (Throwable e) {
+                log.error(MessageUtil.format("trimParam error", e, "field", field.getName(), "param", param));
+            }
+        }
     }
 }
