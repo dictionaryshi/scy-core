6b2c555df96f0b4c81250e0a95503dd2396166b3
史春阳 <shichunyang02@meituan.com>
2020-08-30 10:11:36 +0800

    DateUtil.java

--- src/main/java/com/scy/core/format/DateUtil.java
+++ src/main/java/com/scy/core/format/DateUtil.java
@@ -3,7 +3,10 @@ package com.scy.core.format;
 import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * DateUtil
@@ -39,8 +42,102 @@ public class DateUtil {
     public static final String PATTERN_GMT = "EEE, dd MMM yyyy HH:mm:ss 'GMT'";
     public static final String GMT = "GMT";
 
+    public static int millisecond2Second(long millisecond) {
+        return (int) (millisecond / 1000);
+    }
+
+    public static Date getCurrentDate() {
+        return new Date();
+    }
+
+    public static String getCurrentDateStr() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(PATTERN_SECOND);
+        return dateFormat.format(new Date());
+    }
+
+    public static Date str2Date(String dateStr, String pattern) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        try {
+            return dateFormat.parse(dateStr);
+        } catch (Exception e) {
+            log.error(MessageUtil.format("str2Date error", e, "dateStr", dateStr, "pattern", pattern));
+            return null;
+        }
+    }
+
     public static String date2Str(Date date, String pattern) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
         return dateFormat.format(date);
+    }
+
+    public static Date getDayOffset(Date date, int offset) {
+        return getDateOffset(date, Calendar.DAY_OF_MONTH, offset);
+    }
+
+    public static Date getHourOffset(Date date, int offset) {
+        return getDateOffset(date, Calendar.HOUR_OF_DAY, offset);
+    }
+
+    public static Date getMinuteOffset(Date date, int offset) {
+        return getDateOffset(date, Calendar.MINUTE, offset);
+    }
+
+    public static Date getSecondOffset(Date date, int offset) {
+        return getDateOffset(date, Calendar.SECOND, offset);
+    }
+
+    private static Date getDateOffset(Date date, int field, int offset) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(field, offset);
+        return cal.getTime();
+    }
+
+    public static SimpleDateFormat getGmtSimpleDateFormat() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN_GMT, Locale.US);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone(GMT));
+        return simpleDateFormat;
+    }
+
+    public static Date getDayStartTime(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    public static Date getDayEndTime(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
+        return cal.getTime();
+    }
+
+    public static Date getMonthStartTime(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    public static Date getMonthEndTime(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
+        return cal.getTime();
     }
 }
