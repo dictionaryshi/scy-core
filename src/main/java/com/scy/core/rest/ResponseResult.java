998b8abda17f05a6028aca1a335d1074f77d4797
史春阳 <shichunyang02@meituan.com>
2020-09-02 20:17:25 +0800

    JsonUtil.java

--- src/main/java/com/scy/core/rest/ResponseResult.java
+++ src/main/java/com/scy/core/rest/ResponseResult.java
@@ -5,6 +5,7 @@ import com.scy.core.enums.ResponseCodeEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
@@ -16,21 +17,22 @@ import java.io.Serializable;
  * Created by shichunyang on 2020/8/21.
  */
 @ApiModel("响应结果")
+@NoArgsConstructor
 @Getter
 @ToString
 public class ResponseResult<T> implements Serializable {
 
     @ApiModelProperty(value = "响应状态码(0:成功, 其它:失败)", required = true, example = "0")
-    private final int code;
+    private int code;
 
     @ApiModelProperty(value = "响应状态", required = true, example = "true")
-    private final boolean success;
+    private boolean success;
 
     @ApiModelProperty(value = "错误信息")
-    private final String message;
+    private String message;
 
     @ApiModelProperty(value = "响应数据")
-    private final T data;
+    private T data;
 
     private ResponseResult(int code, String message, T data) {
         this.code = code;
