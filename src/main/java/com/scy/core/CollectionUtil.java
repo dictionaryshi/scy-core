933a50ad7f111987fb7555e491cdab99c35a9317
史春阳 <shichunyang02@meituan.com>
2020-09-03 11:48:19 +0800

    CollectionUtil.java

--- src/main/java/com/scy/core/CollectionUtil.java
+++ src/main/java/com/scy/core/CollectionUtil.java
@@ -153,7 +153,6 @@ public class CollectionUtil {
         return Collections.synchronizedList(list);
     }
 
-
     public static <T> Set<T> synchronizedSet(Set<T> set) {
         return Collections.synchronizedSet(set);
     }
