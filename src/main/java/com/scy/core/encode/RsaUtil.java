055e9bf45bee80361423b9abd6c64ed86bb73d48
史春阳 <shichunyang02@meituan.com>
2020-09-03 19:48:35 +0800

    RsaUtil.java

---
+++ src/main/java/com/scy/core/encode/RsaUtil.java
@@ -0,0 +1,23 @@
+package com.scy.core.encode;
+
+/**
+ * RsaUtil
+ *
+ * @author shichunyang
+ * Created by shichunyang on 2020/9/3.
+ */
+public class RsaUtil {
+
+    private RsaUtil() {
+    }
+
+    /**
+     * 签名类型
+     */
+    public static final String SIGN_TYPE = "RSA";
+
+    /**
+     * 签名算法
+     */
+    public static final String SIGN_ALGORITHMS = "SHA1withRSA";
+}
