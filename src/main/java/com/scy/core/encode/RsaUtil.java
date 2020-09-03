df043f6f75a56adba7be919125bf95f00065b3d7
史春阳 <shichunyang02@meituan.com>
2020-09-03 20:09:56 +0800

    RsaUtil.java

--- src/main/java/com/scy/core/encode/RsaUtil.java
+++ src/main/java/com/scy/core/encode/RsaUtil.java
@@ -1,5 +1,10 @@
 package com.scy.core.encode;
 
+import com.scy.core.model.RsaKeyBO;
+import org.apache.commons.codec.binary.Base64;
+
+import java.security.*;
+
 /**
  * RsaUtil
  *
@@ -20,4 +25,28 @@ public class RsaUtil {
      * 签名算法
      */
     public static final String SIGN_ALGORITHMS = "SHA1withRSA";
+
+    public static final int DEFAULT_LENGTH = 1024;
+
+    public static RsaKeyBO createKey(int length) {
+        KeyPairGenerator keyPairGenerator;
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance(SIGN_TYPE);
+        } catch (Throwable e) {
+            return null;
+        }
+
+        keyPairGenerator.initialize(length);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RsaKeyBO rsaKeyBO = new RsaKeyBO();
+        rsaKeyBO.setLength(length);
+
+        PublicKey publicKey = keyPair.getPublic();
+        rsaKeyBO.setPublicKey(Base64.encodeBase64String(publicKey.getEncoded()));
+
+        PrivateKey privateKey = keyPair.getPrivate();
+        rsaKeyBO.setPrivateKey(Base64.encodeBase64String(privateKey.getEncoded()));
+        return rsaKeyBO;
+    }
 }
