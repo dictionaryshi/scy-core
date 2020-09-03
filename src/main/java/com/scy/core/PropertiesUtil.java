115c9ca2409492c66f07d5a857dddc2683a285d8
史春阳 <shichunyang02@meituan.com>
2020-09-03 16:49:55 +0800

    PropertiesUtil.java

---
+++ src/main/java/com/scy/core/PropertiesUtil.java
@@ -0,0 +1,29 @@
+package com.scy.core;
+
+import com.scy.core.format.MessageUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.util.Properties;
+
+/**
+ * PropertiesUtil
+ *
+ * @author shichunyang
+ * Created by shichunyang on 2020/9/3.
+ */
+@Slf4j
+public class PropertiesUtil {
+
+    private PropertiesUtil() {
+    }
+
+    public static Properties loadAllProperties(String source) {
+        try {
+            return PropertiesLoaderUtils.loadAllProperties(source);
+        } catch (Throwable e) {
+            log.error(MessageUtil.format("loadAllProperties error", "source", source));
+            return null;
+        }
+    }
+}
