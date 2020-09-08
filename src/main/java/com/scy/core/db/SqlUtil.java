48594ef2681c5a45acde4e8d737d34dc936df131
史春阳 <shichunyang02@meituan.com>
2020-09-08 15:42:30 +0800

    SqlUtil.java

--- src/main/java/com/scy/core/db/SqlUtil.java
+++ src/main/java/com/scy/core/db/SqlUtil.java
@@ -1,11 +1,9 @@
 package com.scy.core.db;
 
-import com.scy.core.CollectionUtil;
 import com.scy.core.StringUtil;
 import com.scy.core.SystemUtil;
 
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -16,13 +14,8 @@ import java.util.stream.Stream;
  */
 public class SqlUtil {
 
-    public static <T> String batchInsert(List<T> datas, String table) {
-        if (CollectionUtil.isEmpty(datas)) {
-            return StringUtil.EMPTY;
-        }
+    public static <T> String batchInsert(Class<T> clazz, String table) {
         StringBuilder sb = new StringBuilder();
-        T object = CollectionUtil.firstElement(datas);
-        Class<?> clazz = object.getClass();
         Field[] declaredFields = clazz.getDeclaredFields();
         sb.append("<script>").append(SystemUtil.SYSTEM_LINE_BREAK);
         sb.append("<foreach collection=\"list\" item=\"entry\" separator=\";\">").append(SystemUtil.SYSTEM_LINE_BREAK);
@@ -37,7 +30,7 @@ public class SqlUtil {
         sb.append("<trim prefix=\"values (\" suffix=\")\" suffixOverrides=\",\">").append(SystemUtil.SYSTEM_LINE_BREAK);
         Stream.of(declaredFields).map(Field::getName).forEach(fieldName -> {
             sb.append("<if test=\"entry.").append(fieldName).append(" != null\">").append(SystemUtil.SYSTEM_LINE_BREAK);
-            sb.append("#{").append(fieldName).append("}").append(StringUtil.COMMA).append(SystemUtil.SYSTEM_LINE_BREAK);
+            sb.append("#{").append("entry.").append(fieldName).append("}").append(StringUtil.COMMA).append(SystemUtil.SYSTEM_LINE_BREAK);
             sb.append("</if>").append(SystemUtil.SYSTEM_LINE_BREAK);
         });
         sb.append("</trim>").append(SystemUtil.SYSTEM_LINE_BREAK);
