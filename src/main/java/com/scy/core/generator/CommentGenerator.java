518e43d93169764a8da4e8b0ada1c5ac58793632
史春阳 <shichunyang02@meituan.com>
2020-09-11 21:20:30 +0800

    mybatis generator

--- src/main/java/com/scy/core/generator/CommentGenerator.java
+++ src/main/java/com/scy/core/generator/CommentGenerator.java
@@ -3,11 +3,13 @@ package com.scy.core.generator;
 import com.scy.core.StringUtil;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
-import org.mybatis.generator.api.dom.java.Field;
-import org.mybatis.generator.api.dom.java.Method;
-import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.kotlin.KotlinFile;
 import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * CommentGenerator
@@ -17,8 +19,44 @@ import org.mybatis.generator.internal.DefaultCommentGenerator;
  */
 public class CommentGenerator extends DefaultCommentGenerator {
 
+    public CommentGenerator() {
+        super();
+    }
+
     @Override
-    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+    }
+
+    @Override
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
     }
 
     @Override
@@ -29,15 +67,11 @@ public class CommentGenerator extends DefaultCommentGenerator {
     }
 
     @Override
-    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-    }
-
-    @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
     }
 
     @Override
-    public void addComment(XmlElement xmlElement) {
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
     }
 
     @Override
@@ -46,5 +80,29 @@ public class CommentGenerator extends DefaultCommentGenerator {
 
     @Override
     public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+    }
+
+    @Override
+    public void addFileComment(KotlinFile kotlinFile) {
     }
 }
