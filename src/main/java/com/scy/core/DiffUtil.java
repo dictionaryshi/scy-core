f9da29b0ab76abcd348c5aaa3a625d35bf1ce5a4
史春阳 <shichunyang02@meituan.com>
2021-07-14 14:40:14 +0800

    flatMap

--- src/main/java/com/scy/core/DiffUtil.java
+++ src/main/java/com/scy/core/DiffUtil.java
@@ -7,7 +7,13 @@ import com.scy.core.model.DiffBO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Field;
+import java.util.AbstractMap;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -68,5 +74,32 @@ public class DiffUtil {
 
     public static boolean isDiff(Object before, Object after) {
         return !CollectionUtil.isEmpty(diff(before, after));
+    }
+
+    public static Map<String, Object> flatMap(Map<String, Object> map) {
+        if (CollectionUtil.isEmpty(map)) {
+            return Collections.emptyMap();
+        }
+
+        return map.entrySet().stream()
+                .filter(entry -> Objects.nonNull(entry) && Objects.nonNull(entry.getKey()))
+                .flatMap(DiffUtil::flatEntry)
+                .collect(TreeMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), TreeMap::putAll);
+    }
+
+    private static Stream<Map.Entry<String, Object>> flatEntry(Map.Entry<String, Object> entry) {
+        if (entry.getValue() instanceof Map<?, ?>) {
+            return ((Map<?, ?>) entry.getValue()).entrySet().stream()
+                    .flatMap(e -> flatEntry(new AbstractMap.SimpleEntry<>(entry.getKey() + StringUtil.POINT + e.getKey(), e.getValue())));
+        }
+
+        if (entry.getValue() instanceof List<?>) {
+            List<?> list = (List<?>) entry.getValue();
+            return IntStream.range(0, list.size())
+                    .mapToObj(i -> new AbstractMap.SimpleEntry<String, Object>(entry.getKey() + StringUtil.POINT + i, list.get(i)))
+                    .flatMap(DiffUtil::flatEntry);
+        }
+
+        return Stream.of(entry);
     }
 }
