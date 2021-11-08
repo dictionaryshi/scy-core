8a8028846b6bce2446249ff22d58eef7d8182352
史春阳 <shichunyang02@meituan.com>
2021-11-08 15:26:32 +0800

    SqlUtil.java

--- src/main/java/com/scy/core/db/SqlUtil.java
+++ src/main/java/com/scy/core/db/SqlUtil.java
@@ -1,9 +1,12 @@
 package com.scy.core.db;
 
+import com.scy.core.CollectionUtil;
 import com.scy.core.StringUtil;
 import com.scy.core.SystemUtil;
 
 import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -17,25 +20,27 @@ public class SqlUtil {
     private SqlUtil() {
     }
 
-    public static <T> String batchInsert(Class<T> clazz, String table) {
+    public static <T> String batchInsert(Class<T> clazz, String table, List<String> excludeFields) {
         StringBuilder sb = new StringBuilder();
         Field[] declaredFields = clazz.getDeclaredFields();
+        List<String> fieldNames = Stream.of(declaredFields).map(Field::getName)
+                .filter(fieldName -> !CollectionUtil.emptyIfNull(excludeFields).contains(fieldName)).collect(Collectors.toList());
+
         sb.append("<script>").append(SystemUtil.SYSTEM_LINE_BREAK);
-        sb.append("<foreach collection=\"list\" item=\"entry\" separator=\";\">").append(SystemUtil.SYSTEM_LINE_BREAK);
         sb.append("insert into ").append(table).append(StringUtil.SPACE).append(SystemUtil.SYSTEM_LINE_BREAK);
         sb.append("<trim prefix=\"(\" suffix=\")\" suffixOverrides=\",\">").append(SystemUtil.SYSTEM_LINE_BREAK);
-        Stream.of(declaredFields).map(Field::getName).forEach(fieldName -> {
-            sb.append("<if test=\"entry.").append(fieldName).append(" != null\">").append(SystemUtil.SYSTEM_LINE_BREAK);
-            sb.append(StringUtil.humpToLine(fieldName)).append(StringUtil.COMMA).append(SystemUtil.SYSTEM_LINE_BREAK);
-            sb.append("</if>").append(SystemUtil.SYSTEM_LINE_BREAK);
-        });
+        fieldNames.forEach(
+                fieldName -> sb.append(StringUtil.humpToLine(fieldName)).append(StringUtil.COMMA).append(SystemUtil.SYSTEM_LINE_BREAK)
+        );
         sb.append("</trim>").append(SystemUtil.SYSTEM_LINE_BREAK);
-        sb.append("<trim prefix=\"values (\" suffix=\")\" suffixOverrides=\",\">").append(SystemUtil.SYSTEM_LINE_BREAK);
-        Stream.of(declaredFields).map(Field::getName).forEach(fieldName -> {
-            sb.append("<if test=\"entry.").append(fieldName).append(" != null\">").append(SystemUtil.SYSTEM_LINE_BREAK);
-            sb.append("#{").append("entry.").append(fieldName).append("}").append(StringUtil.COMMA).append(SystemUtil.SYSTEM_LINE_BREAK);
-            sb.append("</if>").append(SystemUtil.SYSTEM_LINE_BREAK);
-        });
+
+        sb.append(" VALUES ").append(SystemUtil.SYSTEM_LINE_BREAK);
+
+        sb.append("<foreach collection=\"list\" item=\"entry\" separator=\",\">").append(SystemUtil.SYSTEM_LINE_BREAK);
+        sb.append("<trim prefix=\"(\" suffix=\")\" suffixOverrides=\",\">").append(SystemUtil.SYSTEM_LINE_BREAK);
+        fieldNames.forEach(
+                fieldName -> sb.append("#{").append("entry.").append(fieldName).append("}").append(StringUtil.COMMA).append(SystemUtil.SYSTEM_LINE_BREAK)
+        );
         sb.append("</trim>").append(SystemUtil.SYSTEM_LINE_BREAK);
         sb.append("</foreach>").append(SystemUtil.SYSTEM_LINE_BREAK);
         sb.append("</script>").append(SystemUtil.SYSTEM_LINE_BREAK);
