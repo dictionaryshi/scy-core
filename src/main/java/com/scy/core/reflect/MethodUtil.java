bffab7535ad4c7f05fbb03cce7cd72980ea6de7f
史春阳 <shichunyang02@meituan.com>
2022-02-19 22:10:17 +0800

    MethodUtil.java

---
+++ src/main/java/com/scy/core/reflect/MethodUtil.java
@@ -0,0 +1,49 @@
+package com.scy.core.reflect;
+
+import com.scy.core.ObjectUtil;
+import com.scy.core.model.MethodParam;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Method;
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/2/19
+ * Time    : 5:39 下午
+ * ---------------------------------------
+ * Desc    : MethodUtil
+ */
+@Slf4j
+public class MethodUtil {
+
+    public static final Map<MethodParam, Method> METHOD_MAP = new ConcurrentHashMap<>();
+
+    public static Method getMethod(Class<?> beanClass, String methodName, Class<?>[] parameterTypes) {
+        MethodParam methodParam = new MethodParam(beanClass, methodName, parameterTypes);
+
+        Method targetMethod = METHOD_MAP.get(methodParam);
+        if (!ObjectUtil.isNull(targetMethod)) {
+            return targetMethod;
+        }
+
+        try {
+            Method method = beanClass.getDeclaredMethod(methodName, parameterTypes);
+            method.setAccessible(Boolean.TRUE);
+
+            METHOD_MAP.putIfAbsent(methodParam, method);
+
+            return method;
+        } catch (Exception e) {
+            log.error(MessageFormat.format("getMethod error", e, "methodName", methodName));
+            return null;
+        }
+    }
+
+    public static void putMethod(Method method) {
+        MethodParam methodParam = new MethodParam(method.getDeclaringClass(), method.getName(), method.getParameterTypes());
+        METHOD_MAP.putIfAbsent(methodParam, method);
+    }
+}
