e22dc48cd95067825faebcb189d346134fdf93d0
史春阳 <shichunyang02@meituan.com>
2022-03-13 19:58:57 +0800

    Route.java

--- src/main/java/com/scy/core/Route.java
+++ src/main/java/com/scy/core/Route.java
@@ -3,9 +3,7 @@ package com.scy.core;
 import com.scy.core.format.DateUtil;
 import com.scy.core.format.NumberUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,6 +20,8 @@ public class Route {
     private static final ConcurrentMap<String, AtomicInteger> COUNTER_MAP = new ConcurrentHashMap<>();
 
     private static long cacheValidTime = 0;
+
+    private static final int VIRTUAL_NODE_NUM = 100;
 
     public static <T> T routeLoop(String serviceKey, TreeSet<T> set) {
         if (CollectionUtil.isEmpty(set)) {
@@ -56,5 +56,29 @@ public class Route {
         int index = RandomUtil.nextInt(0, list.size());
 
         return list.get(index);
+    }
+
+    public static <T> T routeConsistentHash(String serviceKey, TreeSet<T> set) {
+        if (CollectionUtil.isEmpty(set)) {
+            return null;
+        }
+
+        TreeMap<Long, T> nodeMap = new TreeMap<>();
+
+        set.forEach(node -> {
+            for (int i = 0; i < VIRTUAL_NODE_NUM; i++) {
+                long nodeHash = ObjectUtil.hash("SHARD-" + node + "-NODE-" + i);
+                nodeMap.put(nodeHash, node);
+            }
+        });
+
+        long serviceKeyHash = ObjectUtil.hash(serviceKey);
+
+        SortedMap<Long, T> tailMap = nodeMap.tailMap(serviceKeyHash);
+        if (!tailMap.isEmpty()) {
+            return tailMap.get(tailMap.firstKey());
+        }
+
+        return nodeMap.firstEntry().getValue();
     }
 }
