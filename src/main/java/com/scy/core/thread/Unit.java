992a32b8adc1d9acced919b3a0b904eb4b9b07df
史春阳 <shichunyang02@meituan.com>
2022-06-22 15:00:25 +0800

    执行单元设计

---
+++ src/main/java/com/scy/core/thread/Unit.java
@@ -0,0 +1,41 @@
+package com.scy.core.thread;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/6/22
+ * Time    : 11:54 上午
+ * ---------------------------------------
+ * Desc    : 执行单元
+ */
+@Getter
+@Setter
+@ToString
+public class Unit {
+
+    private String id;
+
+    private int priority;
+
+    private String desc;
+
+    private String poolName;
+    private Integer coreSize;
+    private Integer maxSize;
+    private Integer queueSize;
+
+    private String className;
+
+    private String methodName;
+
+    private String[] parameterTypes;
+
+    private boolean status;
+
+    private int code = -1;
+
+    private Throwable throwable;
+}
