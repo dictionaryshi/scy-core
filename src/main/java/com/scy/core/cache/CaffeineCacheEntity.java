ccfec48e2ff6c08d0f2a67ef06b6f45e4fa54f59
shichunyang02 <shichunyang02@meituan.com>
2024-01-23 13:19:43 +0800

    CaffeineCacheEntity.java

--- src/main/java/com/scy/core/cache/CaffeineCacheEntity.java
+++ src/main/java/com/scy/core/cache/CaffeineCacheEntity.java
@@ -1,37 +1,25 @@
 package com.scy.core.cache;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
-/**
- * @author : shichunyang
- * Date    : 2023/1/16
- * Time    : 5:54 下午
- * ---------------------------------------
- * Desc    : CacheEntity
- */
-public class CacheEntity<K, V> {
+public class CaffeineCacheEntity<K, V> {
 
     private final Integer refreshTimeSec;
-
     private final Integer expireTimeSec;
-
     private final Integer initialCapacity;
-
     private final Integer maximumSize;
-
     private final ThreadPoolExecutor threadPool;
-
     private final Function<K, V> sourceCall;
+    private AsyncLoadingCache<K, V> cache;
 
-    private LoadingCache<K, V> cache;
-
-    public CacheEntity(Integer refreshTimeSec, Integer expireTimeSec, Integer initialCapacity, Integer maximumSize, ThreadPoolExecutor threadPool, Function<K, V> sourceCall) {
+    public CaffeineCacheEntity(Integer refreshTimeSec, Integer expireTimeSec, Integer initialCapacity, Integer maximumSize, ThreadPoolExecutor threadPool, Function<K, V> sourceCall) {
         if (refreshTimeSec >= expireTimeSec) {
             throw new IllegalArgumentException("refreshTimeSec must be less than expireTimeSec");
         }
@@ -45,31 +33,25 @@ public class CacheEntity<K, V> {
     }
 
     public void init() {
-        cache = CacheBuilder.newBuilder()
-                // refreshTimeSec要小于expireTimeSec
+        cache = Caffeine.newBuilder()
                 .refreshAfterWrite(refreshTimeSec, TimeUnit.SECONDS)
                 .expireAfterWrite(expireTimeSec, TimeUnit.SECONDS)
                 .initialCapacity(initialCapacity)
                 .maximumSize(maximumSize)
+                .executor(threadPool)
                 .recordStats()
-                .build(
-                        CacheLoader.asyncReloading(new CacheLoader<K, V>() {
-                            @Override
-                            public V load(K key) throws Exception {
-                                return sourceCall.apply(key);
-                            }
-                        }, threadPool)
-                );
+                .buildAsync((key, executor) -> CompletableFuture.supplyAsync(() -> sourceCall.apply(key), executor));
     }
 
-    public V getValue(K key) throws Exception {
-        V value = cache.get(key);
+    public V getValue(K key) {
+        V value = cache.get(key).join();
+        CacheStats stats = cache.synchronous().stats();
         System.out.println(value);
-        System.out.println(cache.stats().hitRate());
+        System.out.println(stats.hitRate());
         return value;
     }
 
-    public V getSourceCall(K key) throws Exception {
+    public V getSourceCall(K key) {
         return sourceCall.apply(key);
     }
 }
